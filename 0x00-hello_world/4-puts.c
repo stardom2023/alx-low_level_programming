cdd5d0d98f1d0d57aa3824e210ba3a8507c60dff
stardom2023 <joy.offor@yahoo.com>
2023-06-16 06:38:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main:the entry point of the program
+ *Return- Always successful(0)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle\n");
+        return (0);
+}
