2f95736d20e425aaf4a7de6ca453187a3d679848
stardom2023 <joy.offor@yahoo.com>
2023-06-16 16:17:31 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main- the entry point of the program
+ *Description: 'print lowercase except q and e'
+ *Return: always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
