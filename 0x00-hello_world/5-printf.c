f08997c1f5bcbe514dcccf0fd56e171c3e67c675
stardom2023 <joy.offor@yahoo.com>
2023-06-16 07:01:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+/**
+ *main- the entry point of our program
+ *Return: Always successful (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
