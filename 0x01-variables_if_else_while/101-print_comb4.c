3e102109a077a5cff6330ecb59e39917eaf0cc54
stardom2023 <joy.offor@yahoo.com>
2023-06-16 20:50:31 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, m, l;
+
+	for (n = 0; n <= 7; n++)
+		{
+		for (m = n + 1; m <= 8; m++)
+		{
+			for (l = m + 1; l <= 9; l++)
+			{
+				putchar(n + '0');
+				putchar(m + '0');
+				putchar(l + '0');
+				if (n != 7 || m != 8 || l != 9)
+			{
+					putchar(',');
+					putchar(' ');
+				}
+				}
+				}
+				}	
+	putchar('\n');
+	return (0);
+}
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				if (n != 7 || m != 8 || l != 9)
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
+}
