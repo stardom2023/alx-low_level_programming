77d67527209dc2142c5d33d4d47bf0d60503eeba
stardom2023 <joy.offor@yahoo.com>
2023-06-16 21:09:14 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - prints all possible different combinations of three digits
+ * main- prints all possible different combinations of three digits
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	int n, m, l;
 
 	for (n = 0; n <= 7; n++)
-		{
+	{
 		for (m = n + 1; m <= 8; m++)
 		{
 			for (l = m + 1; l <= 9; l++)
@@ -17,18 +17,16 @@ int main(void)
 				putchar(m + '0');
 				putchar(l + '0');
 				if (n != 7 || m != 8 || l != 9)
-			{
+				{
 					putchar(',');
 					putchar(' ');
 				}
-				}
-				}
-				}	
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
-				
-				
 				
 				
 				
