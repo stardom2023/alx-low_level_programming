a019ecc574a78b49e213171cc985a1eaf986fb31
stardom2023 <joy.offor@yahoo.com>
2023-06-20 19:43:45 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 
 /**
  * times_table - prints the 9 times table, starting with 0
@@ -23,7 +23,8 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10)
+}
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
