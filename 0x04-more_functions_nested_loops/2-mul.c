dfebd6e61807741c3c99609800a876b2adecbe63
stardom2023 <joy.offor@yahoo.com>
2023-06-22 21:24:13 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *main - entry point
+ *description : "multiply 2 numbers"
+ *Return: 1 for a character that will be a digit or 0 for anything else
+ */
+
+int mul(int x, int y)
+{
+	return (x *y);
+}
