f6352cc51e8b78287821d41ab27ca12625a3973a
stardom2023 <joy.offor@yahoo.com>
2023-06-26 08:12:05 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98- point the variable to a new number 98
+ * @n: this is the character used
+ */
+
+void reset_to_98(int *n)
+{
+	int jay = 98;
+	*n = jay;
+}
