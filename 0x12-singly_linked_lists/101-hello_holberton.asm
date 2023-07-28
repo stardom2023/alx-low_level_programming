f87b6f1f391c4b307832bd758d1b03fae909ba3b
stardom2023 <joy.offor@yahoo.com>
2023-07-27 18:16:11 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,11 @@
-section .text
-Extern printf
-Global main
-Main:
-	Mov edi, msg
-	Mor eax, 0
-	Call printf
-	Section .data
-Msg db 'Hello, Holberton',0xa, 0
+section		.text
+	extern printf
+	global main
+
+main:
+	mov	edi, msg
+	mov	eax, 0
+	call	printf
+
+section		.data
+	msg db 'Hello, Holberton',0xa, 0
