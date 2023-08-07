85ae508ab4ae5c36934ca7c254c134496d618215
stardom2023 <joy.offor@yahoo.com>
2023-08-07 14:41:35 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (lent = 0; text_content[lent];)
-				lent++;
+			lent++;
 	}
 
 	a = open(filename, O_WRONLY | O_APPEND);
--- 0x15-file_io/2-main.c
+++ 0x15-file_io/2-main.c
@@ -18,5 +18,6 @@ int main(int ac, char **av)
 	}
 	res = append_text_to_file(av[1], av[2]);
 	printf("-> %i)\n", res);
+
 	return (0);
 }
