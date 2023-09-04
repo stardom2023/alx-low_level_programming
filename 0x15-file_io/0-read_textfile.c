dafcd2267dddef01df3d08d4ea400ec27988a70f
stardom2023 <joy.offor@yahoo.com>
2023-09-04 14:25:31 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,49 +2,48 @@
 #include <fcntl.h>
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * read_textfile - a function that reads and prints a text file
- * @filename: name of file
- * @letters: characters to read and print
+ * read_textfile - Reads a text file and prints it to standard output
+ * @filename: Name of file to read
+ * @letters: Number of letters to read and print
  * Return: 0
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
-	
-	int file_io;
+{
+	int file_des;
 	ssize_t bytes_read, bytes_written;
-	char *buff;
+	char *buffer;
 
 	if (filename == NULL)
-	return (0);
+		return (0);
 
-	file_io = open(filename, O_RDONLY);
-	if (file_io == -1)
-	return (0);
+	file_des = open(filename, O_RDONLY);
+	if (file_des == -1)
+		return (0);
 
-	buff = malloc(letters);
-	if (buff == NULL)
+	buffer = malloc(letters);
+	if (buffer == NULL)
 	{
-		close(file_io);
+		close(file_des);
 		return (0);
 	}
 
-	bytes_read = read(file_io, buff, letters);
+	bytes_read  = read(file_des, buffer, letters);
 	if (bytes_read == -1)
 	{
-		close(file_io);
-		free(buff);
+		close(file_des);
+		free(buffer);
 		return (0);
 	}
 
-	bytes_written = write(STDOUT_FILENO, buff, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-	close(file_io);
-	free(buff);
+	close(file_des);
+	free(buffer);
+
 	if (bytes_written == bytes_read)
-	return (bytes_written);
+		return (bytes_written);
 
 	return (0);
 }
